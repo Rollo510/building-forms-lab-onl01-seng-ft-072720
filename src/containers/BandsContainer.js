b19aa999e27a1a33ce95208b08436db9e56ad2ae
kbritt510 <kbritt510@gmail.com>
2020-12-10 23:42:48 -0600

    commit tree 8a67f45a334843bd7b42d6b324926542a19e7e45

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import connect from 'react-redux/lib/connect/connect'
+import mapStateToProps from 'react-redux/lib/connect/mapStateToProps'
+
 
 class BandsContainer extends Component {
+
   render() {
     return(
       <div>
         BandsContainer
+      <ul>
+        {this.props.bands}
+      </ul>
       </div>
     )
   }
 }
 
-export default BandsContainer
+export default connect()(BandsContainer)
